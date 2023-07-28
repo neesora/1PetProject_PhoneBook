d4b1476bfdf8b5a9d8fc1bfc854b02ad4bfba63f
Vice <hellxiwd@ya.ru>
2023-07-28 06:39:20 +0500

    commit

--- educationproj/__init__.py
+++ educationproj/__init__.py
@@ -1,9 +1,12 @@
-def main():
-	x,y =2,8
-	
-	if(x < y):
-		st= "x is less than y"
-	print(st)
-	
-if __name__ == "__main__":
-	main()
+def check_triangle(side1, side2, side3):
+
+    if side1 == side2 == side3:
+        print('Triangle is equalateral')
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        print('Triangle is isosceles')
+    else:
+        print('Triangle is scalene')
+side1 = float(input("Enter the length of the first side: "))
+side2 = float(input("Enter the length of the second side: "))
+side3 = float(input("Enter the length of the third side: "))
+check_triangle(side1, side2, side3)
