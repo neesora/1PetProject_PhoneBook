b5c15a5c17b971fe29e570d12b1e693923d12201
Vice <hellxiwd@ya.ru>
2023-07-28 05:53:15 +0500

    commit

---
+++ educationproj/__init__.py
@@ -0,0 +1,11 @@
+def main():
+	x,y =3,21	
+	if(x < y):
+		st= "x is less than y"	
+	elif (x == y):
+		st= "x is same as y"	
+	else:
+		st="x is greater than y"
+	print(st)	
+if __name__ == "__main__":
+	main()
