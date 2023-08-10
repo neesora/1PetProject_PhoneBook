8df07cb02c61ec8fa6afdc819725c05064025954
Vice <hellxiwd@ya.ru>
2023-08-10 16:42:58 +0500

    commit

---
+++ educationproj/functions.py
@@ -0,0 +1,9 @@
+'''def name():
+    b = input()
+    age = 10
+    summary = age + float(b)
+    print('I love big black cocks', summary, 'centimeter')
+name()'''
+def minNum(num1, num2):
+    print(num1 - num2)
+minNum(7, 2)
