ffc0a5d5f232355cd24324eb86d52a9189ef179e
Vice <hellxiwd@ya.ru>
2023-08-10 16:51:20 +0500

    commit

---
+++ educationproj/list_compr.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"] #creating list which values
+
+newlist = [x for x in fruits if "a" in x] #creating list comprehension where "x" searching value in list "fruits" if have letter "a" then back function "x"
+
+print(newlist) #print result
