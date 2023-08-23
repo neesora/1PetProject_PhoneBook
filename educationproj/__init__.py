3bc4a84c47f5fd72c163ce6d34120e981400b406
Vice <hellxiwd@ya.ru>
2023-08-23 10:28:37 +0500

    commit

--- educationproj/__init__.py
+++ educationproj/__init__.py
@@ -7,12 +7,43 @@ def add_contact():
     phone_book[name] = {"Nickname":nickname, "Number":number}
 
 def modify_contact():
-    name = input("Write the exist name: ")
+    name = input("Write the exist name of contact which will edit: ")
     if name in phone_book:
-        #phone_book["nickname"] = input("Write new nickname") here I'm not sure work that or not and left it for tomorrow
-        #phone_book["number"] = input("Write new number") 
-    #else:
-        #print("Name not in the book")
-#choice = input("Text ")
-#if choice == 1:
-    #print(phone_book.items()) here I trynna test but tired and stopped
+        nickname = input("Write new nickname: ") #There have bug when you skip it than crash
+        number = input("Write new number: ")
+    modifying = phone_book[name] #this block needed for replacement exist values to new values
+    if nickname:
+        modifying["Nickname"] = nickname
+    if number:
+        modifying["Number"] = number
+    else:
+        print("Contact was not found")
+def delete_contact():
+    name = input("Write the exist name of contact which will delete: ")
+    if name in phone_book:
+        del phone_book[name]
+    else:
+        print("Contact was not found")
+#def show_contacts():
+
+def main():
+    while True:
+        print("Phone book")
+        print("Write 1 to add new contact.")
+        print("Write 2 to modify exist contact.")
+        print("Write 3 to delete contact.")
+        print("Write 4 to show contacts.")
+        print("Write 5 to stop running.")
+        choice = input("Enter choice ")
+        if choice == "1":
+            add_contact()
+        if choice == "2":
+            modify_contact()
+        if choice == "3":
+            delete_contact()
+        if choice == "4":
+            print(phone_book)
+        if choice == "5":
+            break
+if __name__ == "__main__":
+    main()
